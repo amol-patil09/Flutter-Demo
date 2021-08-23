650dd6430a7ef998bbed6699e2be109de4c67ee6
Amol Patil <amol.patil@ooma.com>
2021-08-23 17:10:21 +0530

    # 2.7-Long list demo

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,37 @@
-import 'package:demo_app/app_screens/home.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: "Exploring UI Widgets",
     home: Scaffold(
       appBar: AppBar(
-        title: Text("My Flutter App"),
+        title: Text("Long List"),
       ),
       body: getListView(),
     )));
 
+List<String> getListElements() {
+  var items = List<String>.generate(100, (index) => "Item ${index}");
+  return items;
+}
+
 Widget getListView() {
+  var listItems = getListElements();
 
-  var listview = ListView(
-    children: [
-      ListTile(
-        leading: Icon(Icons.landscape),
-        title: Text("Landscape"),
-        subtitle: Text("Beatiful view!"),
-        trailing: Icon(Icons.wb_cloudy),
-      ),
-      ListTile(
-        leading: Icon(Icons.phone),
-        title: Text("Phone"),
-        trailing: Icon(Icons.wb_sunny),
-      ),
-      ListTile(
-        leading: Icon(Icons.desktop_windows),
-        title: Text("Windows"),
-        onTap: (){
-          debugPrint("Windows tapped");
-        }
-      ),
-      Container(
-          padding: EdgeInsets.all(20.0),
-          child:Text("New line",style: TextStyle(fontSize: 15.0,color: Colors.black))
-      )
-
-    ],
+  var listview = ListView.builder(
+    itemBuilder: (BuildContext context, int index) {
+      return ListTile(
+        leading: Icon(Icons.arrow_right),
+        title: Text(listItems[index]),
+        onTap: () {
+          Fluttertoast.showToast(
+              msg: "${listItems[index]} was tapped",
+              toastLength: Toast.LENGTH_SHORT,
+              gravity: ToastGravity.SNACKBAR);
+        },
+      );
+    },
   );
   return listview;
 }
