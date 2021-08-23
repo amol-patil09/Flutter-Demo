e2bb6a9ad087964608d26554aee6ce6587a87088
Amol Patil <amol.patil@ooma.com>
2021-08-23 13:32:15 +0530

    # 2.6-List view demo

--- lib/main.dart
+++ lib/main.dart
@@ -8,5 +8,37 @@ void main() => runApp(MaterialApp(
       appBar: AppBar(
         title: Text("My Flutter App"),
       ),
-      body: Home(),
+      body: getListView(),
     )));
+
+Widget getListView() {
+
+  var listview = ListView(
+    children: [
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beatiful view!"),
+        trailing: Icon(Icons.wb_cloudy),
+      ),
+      ListTile(
+        leading: Icon(Icons.phone),
+        title: Text("Phone"),
+        trailing: Icon(Icons.wb_sunny),
+      ),
+      ListTile(
+        leading: Icon(Icons.desktop_windows),
+        title: Text("Windows"),
+        onTap: (){
+          debugPrint("Windows tapped");
+        }
+      ),
+      Container(
+          padding: EdgeInsets.all(20.0),
+          child:Text("New line",style: TextStyle(fontSize: 15.0,color: Colors.black))
+      )
+
+    ],
+  );
+  return listview;
+}
