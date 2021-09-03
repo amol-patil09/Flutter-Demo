98bb0e26478f7bee7d8edae0d48bf21318ec876b
Amol Patil <amol.patil@ooma.com>
2021-09-03 23:05:16 +0530

    #3.3-Design UI for simple calculator app

--- lib/main.dart
+++ lib/main.dart
@@ -1,69 +1,127 @@
-import 'dart:html';
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    title: "Exploring UI Widgets",
-    home: FavoriteCity()));
+    title: "Simple Interest Calculator",
+    home: SIForm()));
 
-class FavoriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _FavoriteCityState();
+    return _SIFormState();
   }
 }
 
-class _FavoriteCityState extends State<FavoriteCity> {
-  String nameCity = "";
+class _SIFormState extends State<SIForm> {
   var _currencies = ['Rupees', 'Pounds', 'Dollars', 'Others'];
-  var _currentItemSelected = 'Dollars';
+  var _currentItemSelected = 'Rupees';
+  var _minimumMargin = 5.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(("Stateful Widget"))),
+      appBar: AppBar(title: Text(("Simple App Calculator"))),
       body: Container(
-        margin: EdgeInsets.all(20.0),
-        child: Column(
+        margin: EdgeInsets.all(_minimumMargin * 2),
+        child: ListView(
           children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: "Enter City name",
-                labelText: "City name",
+            getImageAsset(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumMargin, bottom: _minimumMargin),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Principal",
+                      hintText: "Enter Principal e.g.12000",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumMargin, bottom: _minimumMargin),
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Rate of Interest",
+                      hintText: "Enter Rate of Interest",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5.0))),
+                )),
+            Padding(
+              padding:
+                  EdgeInsets.only(top: _minimumMargin, bottom: _minimumMargin),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                          labelText: "Term",
+                          hintText: "Term",
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    ),
+                  ),
+                  Container(width: _minimumMargin * 5),
+                  Expanded(
+                    child: DropdownButton<String>(
+                      items: _currencies.map((String value) {
+                        return DropdownMenuItem<String>(
+                            value: value, child: Text(value));
+                      }).toList(),
+                      value: _currentItemSelected,
+                      onChanged: (String? newValueSelected) {
+                        _onDropDownItemSelected(newValueSelected!);
+                      },
+                    ),
+                  )
+                ], // Row Children
               ),
-              onChanged: (String userInput) {
-                setState(() {
-                  nameCity = userInput;
-                });
-              },
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String value) {
-                return DropdownMenuItem<String>(
-                    value: value, child: Text(value));
-              }).toList(),
-
-              onChanged: (String? newValueSelected) {
-                return _onDropDownItemSelected(newValueSelected!);
-              },
-
-              value: _currentItemSelected,
-
-              hint: Text("Please select",
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16.0
-              ),),
             ),
             Padding(
-                padding: EdgeInsets.only(top: 10.0),
-                child: Text("Your city is $nameCity",
-                    style: TextStyle(fontSize: 20.0))),
-          ],
+              padding:
+                  EdgeInsets.only(top: _minimumMargin, bottom: _minimumMargin),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Material(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(22.0)),
+                      elevation: 10.0,
+                      color: Colors.lightBlue,
+                      child: MaterialButton(
+                        height: 35.0,
+                        onPressed: () {},
+                        child: Text(
+                          "Calculate",
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: _minimumMargin * 5.0,
+                  ),
+                  Expanded(
+                    child: Material(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(22.0)),
+                      elevation: 10.0,
+                      color: Colors.lightBlue,
+                      child: MaterialButton(
+                        height: 35.0,
+                        onPressed: () {},
+                        child: Text(
+                          "Reset",
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ], // Column Children
         ),
       ),
     );
@@ -73,5 +131,14 @@ class _FavoriteCityState extends State<FavoriteCity> {
     setState(() {
       this._currentItemSelected = newValueSelected;
     });
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(image: assetImage, width: 125.0, height: 125.0);
+    return Container(
+        child: image,
+        margin: EdgeInsets.only(top: _minimumMargin * 5),
+        alignment: Alignment.center);
   }
 }
